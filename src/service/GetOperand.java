cc602233bcd5123885167e6ffd7e2bda7060748b
PaulUkhov <164402643+PaulUkhov@users.noreply.github.com>
2024-09-19 22:19:28 +0300

    Update and rename getOperand.java to GetOperand.java
    GetOperand

--- src/service/GetOperand.java
+++ src/service/GetOperand.java
@@ -2,7 +2,7 @@ package service;
 
 import java.util.Scanner;
 
-public class getOperand {
+public class GetOperand {
     // Метод для получения и проверки оператора
     private static char getOperation(String prompt) {
         Scanner scanner = new Scanner(System.in);
