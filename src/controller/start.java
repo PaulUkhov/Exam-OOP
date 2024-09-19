e282a8f619a11734c83d34d7b386f581d50b3d60
PaulUkhov <164402643+PaulUkhov@users.noreply.github.com>
2024-09-19 21:52:43 +0300

    Delete src directory
    delite

--- src/controller/start.java
+++
@@ -1,5 +0,0 @@
-package controller;
-
-public interface start {
-    void start();
-}
--- src/view/viewBihavior.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-public interface viewBihavior {
-    double getNumber(String prompt);
-    char getOperation(String prompt);
-    void displayResult(double result);
-    void displayMessage(String message);
-}
